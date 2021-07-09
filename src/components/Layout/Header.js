3355ed4d3d4a6db5bc1b6ca4d41612942fa1cab7
cllcnkrt <cllcnkrt@outlook.com>
2021-07-09 21:44:00 +0300

    Header section

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,25 @@
+import { Fragment} from 'react'
+import mealsImage from '../../assets/meals.jpg' 
+import classes from './Header.module.css'
+import HeaderCardButton from './HeaderCardButton'
+
+
+
+function Header(props) {
+    return (
+        
+            <Fragment>
+                    <header className={classes.header}>
+                        <h1>FoodOrder</h1>
+                        <HeaderCardButton/>
+                    </header>
+
+                    <div className={classes['main-image']}>
+                        <img src={mealsImage} alt="A table full of delicious meals" />
+                    </div>
+            </Fragment>
+        
+    )
+}
+
+export default Header
